8c914eaa969489795831c209778707df7a3302ad
xmxshr <xmxshr@me.com>
2017-11-15 16:04:56 +0800

    使用scss

--- src/App.vue
+++ src/App.vue
@@ -9,42 +9,42 @@
 </template>
 
 <script>
-import Topbar from './components/Topbar'
-import Editor from './components/Editor'
-import Preview from './components/Preview'
-
+import Topbar from "./components/Topbar";
+import Editor from "./components/Editor";
+import Preview from "./components/Preview";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Topbar, Editor, Preview
+    Topbar,
+    Editor,
+    Preview
   }
-}
+};
 </script>
 
-<style>
-html,body,#app{
+<style lang="scss">
+html,
+body,
+#app {
   height: 100%;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   display: flex;
   flex-direction: column;
-}
-.topbar{
- 
-}
-main{
-  display: flex;
-  flex: 1;
-}
-.preview{
-  flex: 1;
-  margin: 16px;
-}
-.editor{
-  margin: 16px 0 16px 16px;
+  main {
+    display: flex;
+    flex: 1;
+    .preview {
+      flex: 1;
+      margin: 16px;
+    }
+    .editor {
+      margin: 16px 0 16px 16px;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import './assets/reset.css'
+import './assets/reset.scss'
 
 Vue.config.productionTip = false
 
